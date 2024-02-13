40075180bcd61939806eddf596c2f7842f7825b1
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-02-13 19:58:44 +0100

    Update App.js
    added some clickable elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.scss';
 
 function App() {
+
+  const handleUp = () => {
+    console.log('Up');
+  }
+
+  const handleMiddle = () => {
+    console.log('Mid');
+  }
+
+  const handleDown = () => {
+    console.log('Down');
+  }
+
   return (
     <>
-      <main className='app'>
-        Something
-      </main>
+      <div className='app'>
+        <div className='controls-container'>
+            <div className='control' onClick={handleUp}>Up</div>
+            <div className='control' onClick={handleMiddle}>Mid</div>
+            <div className='control' onClick={handleDown}>Down</div>
+        </div>
+      </div>
     </>
   );
 }
