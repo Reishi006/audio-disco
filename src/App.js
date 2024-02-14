b16cafde619580e20d8857cbd1364fb59390cf4d
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-02-14 11:53:37 +0100

    Update App.js
    created an upAnim animation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useState, useEffect, useRef } from 'react';
+
 import './App.scss';
 
 function App() {
 
+  const [isAnimating, setIsAnimating] = useState(false);
+  const upAnim = useRef(null);
+
   const handleUp = () => {
+    const el = upAnim.current;
+
+    el.style.display = `flex`;
+    el.style.animation = 'none';
+    void el.offsetWidth;
+    el.style.animation = '.5s fading 1 forwards';
+
+    setTimeout(() => {
+      upAnim.current.style.display = `none`;
+      console.log(`display`);
+      setIsAnimating(false);
+    },  500);
     console.log('Up');
   }
 
@@ -21,6 +38,16 @@ function App() {
             <div className='control' onClick={handleUp}>Up</div>
             <div className='control' onClick={handleMiddle}>Mid</div>
             <div className='control' onClick={handleDown}>Down</div>
+        </div>
+
+        <div className='anim-container'>
+          <div
+            ref={upAnim} 
+            className={`up-anim`}
+            /* style={{ display: isAnimating ? 'none' : 'flex' }} */
+          >Up</div>
+          {/* <div className='mid-anim'>Mid</div>
+          <div className='down-anim'>Down</div> */}
         </div>
       </div>
     </>
