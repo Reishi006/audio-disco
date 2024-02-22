97546dc469da620e62983f94458d1d0488edf018
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-02-22 21:28:44 +0100

    Update Canvas.js
    refactored the canvas

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,19 +1,42 @@
-import { useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 function Canvas(props) {
     const canvasRef = useRef(null);
+    const [ctx, setCtx] = useState(null);
+
+    const draw = () => {
+        ctx.fillStyle = 'rgb(50, 192, 192)';
+        ctx.filter = `hue-rotate(${props.huerotate}deg)`;
+        ctx.fillRect(0, 0, 100, 100);
+    }
 
     useEffect(() => {
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
+        if (canvasRef.current) {
+            const canvas = canvasRef.current;
+            const context = canvas.getContext('2d');
+            setCtx(context);
+        }
     }, []);
+
+    useEffect(() => {
+        let frameId;
+
+        if (ctx) {
+            const render = () => {
+                draw();
+                frameId = window.requestAnimationFrame(render);
+            }
+            render();
+        }
+        return () => {
+            window.cancelAnimationFrame(frameId);
+        }
+    }, [draw, ctx]);
 
     return (
     <canvas 
         ref={canvasRef} 
-        width={props.width} 
-        height={props.height}
-        style={props.display}
+        {...props}
     />
     );
 }
